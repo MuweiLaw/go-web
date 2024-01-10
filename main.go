5801cd90411f47dbefb23c9a7c0ddef0fbc1f38b
Muwei-Luo <2562758836@qq.com>
2024-01-10 16:39:49 +0800

    feat:成功连上mySQL数据库

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	//db, _ := sql.Open("mysql", "root:admin123456@tcp(localhost:3306)/auth?charset=utf8mb4&parseTime=True")
-	//if err := db.Ping(); err != nil {
-	//	fmt.Println("连接失败")
-	//}
-	//fmt.Println("连接成功")
+	db, _ := sql.Open("mysql", "root:admin123456@tcp(localhost:3306)/auth?charset=utf8mb4&parseTime=True")
+	if err := db.Ping(); err != nil {
+		fmt.Println("连接失败")
+	}
+	fmt.Println("连接成功")
 
 	// 1.创建路由
 	r := gin.Default()
 
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
-	r.Run(":8000")
+	err := r.Run(":8000")
+	if err != nil {
+		return
+	}
 }
